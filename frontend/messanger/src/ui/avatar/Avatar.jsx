e58e1df1506184a57a987874a2b8a876a1154be3
someDev01 <dmitryprogger@mail.ru>
2026-05-27 20:21:27 +0300

    n

--- frontend/messanger/src/ui/avatar/Avatar.jsx
+++ frontend/messanger/src/ui/avatar/Avatar.jsx
@@ -1,19 +1,22 @@
 import styles from '../avatar/avatar.module.css';
 const API = import.meta.env.VITE_API_URL;
 
-function Avatar({avatar, name}){
-    return(
+function Avatar({ avatar, name }) {
+    return (
         <div className={styles.icon}>
             {avatar ? (
                 <img
+                    className={styles.image}
                     src={`${API}${avatar}`}
                     alt="avatar"
                 />
             ) : (
-                name[0]
+                <span className={styles.fallback}>
+                    {name?.[0] || '?'}
+                </span>
             )}
         </div>
-    )
+    );
 }
 
 export default Avatar;
