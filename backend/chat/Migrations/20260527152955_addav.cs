8c4ec194fabf971011695824a05fe28f640ce493
someDev01 <dmitryprogger@mail.ru>
2026-05-27 18:30:08 +0300

    mm

---
+++ backend/chat/Migrations/20260527152955_addav.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace chat.Migrations
+{
+    /// <inheritdoc />
+    public partial class addav : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Users");
+        }
+    }
+}
